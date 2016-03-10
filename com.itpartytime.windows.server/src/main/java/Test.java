1deb06bef7817650188a6082072375393e5caae6
Mincho Velev <velev.mincho@gmail.com>
2016-03-10 19:33:45 +0200

    Deleted test misscommitted test files.

--- com.itpartytime.windows.server/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import com.partytime.windows.server.IMediaController;
-import com.partytime.windows.server.impl.MediaController;
-
-public class Test {
-	public static void main(String[] args) {
-		IMediaController mediaController = new MediaController();
-		mediaController.increaseVolume();
-	}
-}
