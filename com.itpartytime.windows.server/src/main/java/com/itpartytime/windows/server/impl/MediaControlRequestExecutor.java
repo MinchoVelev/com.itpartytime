0abf9d6273053b807f189c92991477f8b0c01f09
Mincho Velev <velev.mincho@gmail.com>
2016-03-29 08:06:50 +0300

    Added basic logging.

--- com.itpartytime.windows.server/src/main/java/com/itpartytime/windows/server/impl/MediaControlRequestExecutor.java
+++ com.itpartytime.windows.server/src/main/java/com/itpartytime/windows/server/impl/MediaControlRequestExecutor.java
@@ -6,22 +6,33 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.Charset;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.itpartytime.common.Commands;
 import com.itpartytime.common.Constants;
 import com.itpartytime.windows.server.IMediaController;
 
 public class MediaControlRequestExecutor extends Thread {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(MediaControlRequestExecutor.class);
+
 	private Socket socket;
+	private String hostName;
 	private boolean active;
 
 	public MediaControlRequestExecutor(Socket socket) {
 		this.socket = socket;
+		hostName = socket.getInetAddress().getHostName();
 		active = true;
 	}
 
 	@Override
 	public void run() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Host '{}' connected.", hostName);
+		}
 		try {
 			IMediaController mediaController = new MediaController();
 
@@ -31,7 +42,10 @@ public class MediaControlRequestExecutor extends Thread {
 
 			while (active) {
 				String command = bufferedReader.readLine();
-				System.out.println("Recieved command: " + command);
+				if (logger.isDebugEnabled()) {
+					logger.debug("Recieved command: '{}' from host: '{}'",
+							command, hostName);
+				}
 				switch (command) {
 
 				case Commands.VOLUME_UP:
@@ -57,14 +71,23 @@ public class MediaControlRequestExecutor extends Thread {
 				case Commands.EXIT:
 					active = false;
 					socket.close();
+					if (logger.isDebugEnabled()) {
+						logger.debug("Host '{}' disconnected via command.",
+								hostName);
+					}
 					break;
 
 				default:
-					System.out.println("unknown command: " + command);
+					if (logger.isDebugEnabled()) {
+						logger.debug("Unknown command: '{}' from host '{}'",
+								command, hostName);
+					}
 				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			if (logger.isDebugEnabled()) {
+				logger.debug("Couldn't process command. ", e);
+			}
 		}
 	}
 }
--- com.itpartytime.windows.server/src/main/java/com/itpartytime/windows/server/impl/MediaControlServer.java
+++ com.itpartytime.windows.server/src/main/java/com/itpartytime/windows/server/impl/MediaControlServer.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MediaControlServer {
+	private static final Logger logger = LoggerFactory
+			.getLogger(MediaControlServer.class);
+
 	private static final int SERVER_PORT = 44455;
 	public static boolean run = true;
 
 	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+		logger.info("Starting server on port {}..", SERVER_PORT);
 		ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-		System.out.println("Starting server on port " + SERVER_PORT);
+		logger.info("Server started.");
 		while (run) {
 			Socket clientSocket = serverSocket.accept();
 			MediaControlRequestExecutor requestExecutor = new MediaControlRequestExecutor(
